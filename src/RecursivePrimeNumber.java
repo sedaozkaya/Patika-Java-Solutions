9918b93a97739629bfcad09c307a22f848cddea7
Seda <230316033@ogr.cbu.edu.tr>
2024-12-04 12:44:15 +0300

    completed

---
+++ src/RecursivePrimeNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class RecursivePrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        while (true) {
+            System.out.print("Sayı Giriniz: ");
+            int num = inp.nextInt();
+
+            if (isPrime(num, 2)) {
+                System.out.println(num + " sayısı ASALDIR!");
+            } else {
+                System.out.println(num + " sayısı ASAL değildir!");
+            }
+        }
+    }
+
+
+    public static boolean isPrime(int num, int divisor) {
+        if (num < 2) {
+            return false;
+        }
+        if (divisor == num) {
+            return true;
+        }
+        if (num % divisor == 0) {
+            return false; r
+        }
+        return isPrime(num, divisor + 1);
+    }
+}
